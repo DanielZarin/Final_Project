26d96e5dbcdd67b249189fd1e6c7a79941497a0a
Daniel Zarin <Daniel Zarin@Daniels-Laptop.mshome.net>
2021-10-07 11:19:30 +0300

    improve tests with arr and loops 07.10.2021

--- Final Project/src/Testing.java
+++ Final Project/src/Testing.java
@@ -81,57 +81,9 @@ public class Testing {
 		@Test
 		public void test5() {
 //Click on the Back button on the answers to question 2
-			WebDriver driver = new ChromeDriver();
-			driver.get(globURL);
-			driver.findElement(By.id("startB")).click();
-			driver.findElement(By.name("question")).sendKeys("a");
-			driver.findElement(By.id("nextquest")).click();
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[1]/div[2]/input")).sendKeys("a");
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[2]/div[2]/input")).sendKeys("b");
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[3]/div[2]/input")).sendKeys("c");
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[4]/div[2]/input")).sendKeys("d");
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[1]/div[1]/input")).click();
-			driver.findElement(By.id("nextquest")).click();
-			driver.findElement(By.name("question")).sendKeys("b");
-			driver.findElement(By.id("nextquest")).click();
-			driver.findElement(By.id("backquest")).click();
-			assertEquals(true, driver.getPageSource().contains("question number: 2")==true);
-		}
-		
-		@Test
-		public void test6() {
-//Click on the Back button on question 3
-			WebDriver driver = new ChromeDriver();
-			driver.get(globURL);
-			driver.findElement(By.id("startB")).click();
-			driver.findElement(By.name("question")).sendKeys("a");
-			driver.findElement(By.id("nextquest")).click();
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[1]/div[2]/input")).sendKeys("a");
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[2]/div[2]/input")).sendKeys("b");
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[3]/div[2]/input")).sendKeys("c");
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[4]/div[2]/input")).sendKeys("d");
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[1]/div[1]/input")).click();
-			driver.findElement(By.id("nextquest")).click();
-			driver.findElement(By.name("question")).sendKeys("b");
-			driver.findElement(By.id("nextquest")).click();
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[1]/div[2]/input")).sendKeys("e");
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[2]/div[2]/input")).sendKeys("f");
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[3]/div[2]/input")).sendKeys("g");
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[4]/div[2]/input")).sendKeys("h");
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[1]/div[1]/input")).click();
-			driver.findElement(By.id("nextquest")).click();
-			driver.findElement(By.id("backquest")).click();
-			assertEquals(true, driver.findElement(By.name("answer1")).isDisplayed());
-			//assertEquals(true, driver.getPageSource().contains("Please enter 4 possible answers and Mark the right one!\r\n" + "question number: 2")==true);
-		}
-		
-		@Test
-		public void test7() {
-//Click on the Back button on the answers to question 3
 			String[] qArr = {"a","b"};
-			String[][] ar = {{"1", "2", "a"},{"2", "2", "b"}, {"3", "2", "c"},{"4", "2", "d"},{"1","2","e"},{"2","2","f"},{"3","2","g"},{"4","2","h"}};
+			String[][] ar = {{"1", "2", "a"},{"2", "2", "b"}, {"3", "2", "c"},{"4", "2", "d"}};
 			int brakein = 4;
-			
 			WebDriver driver = new ChromeDriver();
 			driver.get(globURL);
 			driver.findElement(By.id("startB")).click();
@@ -149,48 +101,100 @@ public class Testing {
 						tmpIndx = 4;
 						break;
 					}
-					
 				}
+			}
+			driver.findElement(By.id("backquest")).click();
+			assertEquals(true, driver.getPageSource().contains("question number: 2")==true);
+		}
+		
+		@Test
+		public void test6() {
+//Click on the Back button on question 3
+			String[] qArr = {"a","b"};
+			String[][] ar = {{"1", "2", "a"},{"2", "2", "b"}, {"3", "2", "c"},{"4", "2", "d"},{"1","2","e"},{"2","2","f"},{"3","2","g"},{"4","2","h"}};
+			int brakein = 4;
+			WebDriver driver = new ChromeDriver();
+			driver.get(globURL);
+			driver.findElement(By.id("startB")).click();
+			int tmpIndx = 0;
+			for (int i = 0; i < qArr.length; i++) {
+				driver.findElement(By.name("question")).sendKeys(qArr[i]);
+				driver.findElement(By.id("nextquest")).click();
 				
+				for (int indx = tmpIndx; indx < ar.length; indx ++) {
+					driver.findElement(By.xpath("//*[@id=\"answers\"]/div["+ ar[indx][0] +"]/div["+ ar[indx][1] +"]/input")).sendKeys(ar[indx][2]);
+					
+					if (((indx + 1) % brakein) == 0) {
+						driver.findElement(By.xpath("//*[@id=\"answers\"]/div[1]/div[1]/input")).click();
+						driver.findElement(By.id("nextquest")).click();
+						tmpIndx = 4;
+						break;
+					}
+				}
+			}
+			driver.findElement(By.id("backquest")).click();
+			assertEquals(true, driver.findElement(By.name("answer1")).isDisplayed());
+		}
+		
+		@Test
+		public void test7() {
+//Click on the Back button on the answers to question 3
+			String[] qArr = {"a","b"};
+			String[][] ar = {{"1", "2", "a"},{"2", "2", "b"}, {"3", "2", "c"},{"4", "2", "d"},{"1","2","e"},{"2","2","f"},{"3","2","g"},{"4","2","h"}};
+			int brakein = 4;
+			WebDriver driver = new ChromeDriver();
+			driver.get(globURL);
+			driver.findElement(By.id("startB")).click();
+			int tmpIndx = 0;
+			for (int i = 0; i < qArr.length; i++) {
+				driver.findElement(By.name("question")).sendKeys(qArr[i]);
+				driver.findElement(By.id("nextquest")).click();
+				
+				for (int indx = tmpIndx; indx < ar.length; indx ++) {
+					driver.findElement(By.xpath("//*[@id=\"answers\"]/div["+ ar[indx][0] +"]/div["+ ar[indx][1] +"]/input")).sendKeys(ar[indx][2]);
+					
+					if (((indx + 1) % brakein) == 0) {
+						driver.findElement(By.xpath("//*[@id=\"answers\"]/div[1]/div[1]/input")).click();
+						driver.findElement(By.id("nextquest")).click();
+						tmpIndx = 4;
+						break;
+					}
+				}	
 			}
 			driver.findElement(By.id("nextquest")).click();
 			driver.findElement(By.name("question")).sendKeys("c");
 			driver.findElement(By.id("nextquest")).click();
 			driver.findElement(By.id("backquest")).click();
 			assertEquals(true, driver.findElement(By.name("question")).isDisplayed());
-		
 		}
 		
 		@Test
 		public void test8() {
 //Click on the Quit button
+			String[] qArr = {"a","b","c"};
+			String[][] ar = {{"1", "2", "a"},{"2", "2", "b"}, {"3", "2", "c"},{"4", "2", "d"},{"1","2","e"},{"2","2","f"},{"3","2","g"},{"4","2","h"},{"1","2","i"},{"2","2","j"},{"3","2","k"},{"4","2","l"}};
+			int brakein = 4;
+			int loop = 1;
 			WebDriver driver = new ChromeDriver();
 			driver.get(globURL);
 			driver.findElement(By.id("startB")).click();
-			driver.findElement(By.name("question")).sendKeys("a");
-			driver.findElement(By.id("nextquest")).click();
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[1]/div[2]/input")).sendKeys("a");
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[2]/div[2]/input")).sendKeys("b");
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[3]/div[2]/input")).sendKeys("c");
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[4]/div[2]/input")).sendKeys("d");
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[1]/div[1]/input")).click();
-			driver.findElement(By.id("nextquest")).click();
-			driver.findElement(By.name("question")).sendKeys("b");
-			driver.findElement(By.id("nextquest")).click();
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[1]/div[2]/input")).sendKeys("e");
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[2]/div[2]/input")).sendKeys("f");
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[3]/div[2]/input")).sendKeys("g");
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[4]/div[2]/input")).sendKeys("h");
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[1]/div[1]/input")).click();
-			driver.findElement(By.id("nextquest")).click();
-			driver.findElement(By.name("question")).sendKeys("c");
-			driver.findElement(By.id("nextquest")).click();
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[1]/div[2]/input")).sendKeys("i");
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[2]/div[2]/input")).sendKeys("j");
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[3]/div[2]/input")).sendKeys("k");
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[4]/div[2]/input")).sendKeys("l");
-			driver.findElement(By.xpath("//*[@id=\"answers\"]/div[1]/div[1]/input")).click();
-			driver.findElement(By.id("nextquest")).click();
+			int tmpIndx = 0;
+			for (int i = 0; i < qArr.length; i++) {
+				driver.findElement(By.name("question")).sendKeys(qArr[i]);
+				driver.findElement(By.id("nextquest")).click();
+				
+				for (int indx = tmpIndx; indx < ar.length; indx ++) {
+					driver.findElement(By.xpath("//*[@id=\"answers\"]/div["+ ar[indx][0] +"]/div["+ ar[indx][1] +"]/input")).sendKeys(ar[indx][2]);
+					
+					if (((indx + 1) % brakein) == 0) {
+						driver.findElement(By.xpath("//*[@id=\"answers\"]/div[1]/div[1]/input")).click();
+						driver.findElement(By.id("nextquest")).click();
+						tmpIndx = 4*loop;
+						loop++;
+						break;
+					}
+				}
+			}
 			driver.findElement(By.xpath("//*[@id=\"secondepage\"]/center/button[2]")).click();
 			assertEquals(false, driver.findElement(By.xpath("//*[@id=\"secondepage\"]/center/button[2]")).isDisplayed());
 		}
