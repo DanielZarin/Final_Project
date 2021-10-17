4ed2937f727e07cb628516ba35186ae104521dd7
Daniel Zarin <Daniel Zarin@Daniels-Laptop.mshome.net>
2021-10-17 16:09:15 +0300

    final fix

--- Final Project/src/Main.java
+++ Final Project/src/Main.java
@@ -2,7 +2,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
 public class Main {
 	
 	public static void main(String[] args) throws Exception {
